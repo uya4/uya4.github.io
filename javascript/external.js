1316f40321998dc7735607440ced57e85ea84954
Ceri <uya4@txstate.edu>
2026-09-15 23:50:55 -0500

    added new variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,11 +1,26 @@
 // document.write("Where is my JS code?");
+
 // window.alert(5 + 6);
 //window.alert('5 + 6');
+
 //document.write('<h1>A heading</h1>');
 //document.write('<p>A sentence.</p>');
+
 //const x = "web";
 //const y = "mapping";
 //const out = x + y;
 //document.write(out);
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+
+//user_name = window.prompt("Please enter your name", "Type your name here");
+//document.write(user_name);
+
+a = 22;
+const b = 33;
+
+document.writeln(a + b);
+document.writeln("<br>");
+
+document.writeln(a += b);
+document.writeln("<br>");
+
+document.writeln(a += b);
