a9ea0781c0a0ca22c872fd7b48a0158656a36ab9
Ceri <uya4@txstate.edu>
2026-09-15 23:26:18 -0500

    added user input asking for name

--- javascript/external.js
+++ javascript/external.js
@@ -3,7 +3,9 @@
 //window.alert('5 + 6');
 //document.write('<h1>A heading</h1>');
 //document.write('<p>A sentence.</p>');
-const x = "web";
-const y = "mapping";
-const out = x + y;
-document.write(out);
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out);
+user_name = window.prompt("Please enter your name", "Type your name here");
+document.write(user_name);
