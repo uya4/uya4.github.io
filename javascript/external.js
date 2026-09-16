879195c22cfe803ded76985c23b63249ca1bd0fe
Ceri <uya4@txstate.edu>
2026-09-15 23:19:42 -0500

    added variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,9 @@
 // document.write("Where is my JS code?");
 // window.alert(5 + 6);
 //window.alert('5 + 6');
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+//document.write('<h1>A heading</h1>');
+//document.write('<p>A sentence.</p>');
+const x = "web";
+const y = "mapping";
+const out = x + y;
+document.write(out);
