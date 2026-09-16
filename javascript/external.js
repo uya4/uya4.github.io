14051c5cfb0c9c99bad87f454d7fde1d2e2faf26
Ceri <uya4@txstate.edu>
2026-09-15 23:54:50 -0500

    deleted practice code and added conditional statement

--- javascript/external.js
+++ javascript/external.js
@@ -1,26 +1,15 @@
-// document.write("Where is my JS code?");
+document.writeln("<button onclick='condition()'>Conditional Test</button>");
 
-// window.alert(5 + 6);
-//window.alert('5 + 6');
+function condition()
+{
+    x = confirm("Are you sure you want to proceed?");
 
-//document.write('<h1>A heading</h1>');
-//document.write('<p>A sentence.</p>');
-
-//const x = "web";
-//const y = "mapping";
-//const out = x + y;
-//document.write(out);
-
-//user_name = window.prompt("Please enter your name", "Type your name here");
-//document.write(user_name);
-
-a = 22;
-const b = 33;
-
-document.writeln(a + b);
-document.writeln("<br>");
-
-document.writeln(a += b);
-document.writeln("<br>");
-
-document.writeln(a += b);
+    if(x)
+    {
+        document.writeln("You chose Okay!");
+    }
+    else
+    {
+        document.writeln("You chose Cancel!");
+    }
+}
