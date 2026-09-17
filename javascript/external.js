32f597bc98910928a8524bbaeb839591b4d4ec42
Ceri <uya4@txstate.edu>
2026-09-16 19:38:17 -0500

    added function

--- javascript/external.js
+++ javascript/external.js
@@ -1,2 +1,19 @@
+/*
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write("<h1>Hello, welcome " + user_name + "!</h1>");
+*/
+
+function welcome()
+{
+    let a = "Please enter your name.";
+    let b = "Type your name here.";
+
+    // A prompt box is used to prompt users to input a value before entering a page.
+    user_name = window.prompt(a, b);
+
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>";
+
+    return message;
+}
+
+document.write(welcome());
