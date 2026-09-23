dacd91d36c74b6327ed81e6e683faf2cc4b9cf82
Ceri <uya4@txstate.edu>
2026-09-23 00:24:15 -0500

    Automated table creation for webmap reviews.

--- javascript/external.js
+++ javascript/external.js
@@ -15,3 +15,24 @@ function welcome()
 
     return message;
 }
+
+function webmap_table()
+{
+    document.write("<table width=100%>");
+
+    for (var row=0; row < 2; row++)
+    {
+        document.write("<tr>");
+
+        for (var column=0; column < 3; column++)
+        {
+            document.write("<td>" + row + "," + column + "</td>");
+        }
+
+        document.write("</tr>");
+    }
+
+    document.write("</table>");
+
+    return "";
+}
