c68c6256415bee154c14e39ddf672a1085b93e68
Ceri <uya4@txstate.edu>
2026-09-22 23:44:05 -0500

    edited error

--- javascript/external.js
+++ javascript/external.js
@@ -11,7 +11,7 @@ function welcome()
     // A prompt box is used to prompt users to input a value before entering a page.
     user_name = window.prompt(a, b);
 
-    let message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>";
+    message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>";
 
     return message;
 }
